8e30a82a2b94fd648633c3ba8132d4bd4be2f8ff
Maksim <maksim_belotelov@mail.ru>
2022-11-24 23:19:35 +0300

    add Ex04 and Ex06

---
+++ Ex06/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа на вход принимает число и выдает, является ли оно четным
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if(num%2!=0) Console.WriteLine("Число является нечетным.");
+else Console.WriteLine("Число является четным.");
