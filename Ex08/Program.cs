2ac9ee008872eee407788d0a9b1e018e1d6c9233
Maksim <maksim_belotelov@mail.ru>
2022-11-24 23:31:54 +0300

    add Ex08

---
+++ Ex08/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа принимает число и на выходе показывает все четные
+// от 1 до N
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.Write($"{num} -> ");
+int i=2;
+
+while(i<=num)
+{
+    if(i%2==0) Console.Write($" {i} ");
+    i++;
+}
